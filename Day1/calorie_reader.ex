5c156960d946e5a6411cdda174a37cfe443b1a49
FinestGit <chaserogness@gmail.com>
2023-06-06 16:11:22 -0500

    Wanting to add specs to each function

--- Day1/calorie_reader.ex
+++ Day1/calorie_reader.ex
@@ -14,26 +14,32 @@ defmodule ReadFile do
     |> IO.inspect()
   end
 
+  @spec accumulateIntegerCalories([integer]) :: integer
   defp accumulateIntegerCalories(list) do
     accumulateIntegerCalories(list, 0)
   end
 
+  @spec accumulateIntegerCalories([], integer) :: integer
   defp accumulateIntegerCalories([], acc) do
     acc
   end
 
+  @spec accumulateIntegerCalories([integer | [integer]], integer) :: integer
   defp accumulateIntegerCalories([head | tail], acc) do
     accumulateIntegerCalories(tail, acc + head)
   end
 
+  @spec accumulateCalories([String.t()]) :: [integer]
   defp accumulateCalories(list) do
     accumulateCalories(list, 0, [])
   end
 
+  @spec accumulateCalories([], integer, [integer]) :: [integer]
   defp accumulateCalories([], acc, list) do
     [acc | list]
   end
 
+  @spec accumulateCalories([String.t() | [String.t()]], integer, [integer]) :: [integer]
   defp accumulateCalories([head | tail], acc, list) do
     first_element = String.split(head, "\r") |> List.first()
 
@@ -44,14 +50,17 @@ defmodule ReadFile do
     end
   end
 
+  @spec findLargestCalories([integer]) :: integer
   defp findLargestCalories(list) do
     findLargestCalories(list, 0)
   end
 
+  @spec findLargestCalories([], integer) :: integer
   defp findLargestCalories([], largest) do
     largest
   end
 
+  @spec findLargestCalories([integer | [integer]], integer) :: integer
   defp findLargestCalories([head | tail], largest) do
     if head > largest do
       findLargestCalories(tail, head)
@@ -60,14 +69,17 @@ defmodule ReadFile do
     end
   end
 
+  @spec findLargestCalories([integer]) :: [integer]
   defp findLargestThreeCalories(list) do
     findLargestThreeCalories(list, [0, 0, 0])
   end
 
+  @spec findLargestCalories([], [integer]) :: [integer]
   defp findLargestThreeCalories([], largestCaloriesList) do
     largestCaloriesList
   end
 
+  @spec findLargestCalories([integer | [integer]], [integer]) :: [integer]
   defp findLargestThreeCalories([head | tail], largestCaloriesList) do
     {smallestCalories, smallestCaloriesIndex} = Enum.with_index(largestCaloriesList) |> Enum.min_by(fn {element, _index} -> element end)
     if head > smallestCalories do
